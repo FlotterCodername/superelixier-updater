fca765a9dc3f0aa1755b1399b87d96b89217a080
FroyoXSG <4962476+FroyoXSG@users.noreply.github.com>
2021-04-25 20:56:03 +0200

    Build zip release

--- build.bat
+++ build.bat
@@ -1,1 +1,13 @@
 pyinstaller main.py -F
+rmdir build /s /q
+xcopy config dist\config /E /Y /I
+del dist\config\local.json
+del dist\config\eula.json
+xcopy docs dist\docs /E /Y /I
+xcopy bin-win32 dist\bin-win32 /E /Y /I
+cd dist
+del ..\..\superelixier-releases\superelixier-updater.zip
+7z -tzip u ..\..\superelixier-releases\superelixier-updater.zip * -m0=Deflate -mx9
+cd ..
+rmdir dist /s /q
+exit
