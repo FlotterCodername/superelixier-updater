28862c52a05edc632eed0ba88f44657ca1cec0dc
FroyoXSG <4962476+FroyoXSG@users.noreply.github.com>
2021-04-26 09:25:27 +0200

    autodoc.py

---
+++ autodoc.py
@@ -0,0 +1,11 @@
+"""
+Copyright 2021 Fabian H. Schneider
+
+This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+If a copy of the MPL was not distributed with this file,
+You can obtain one at https://mozilla.org/MPL/2.0/.
+"""
+from config_loader.config_loader import ConfigLoader
+
+my_cfg = ConfigLoader()
+my_cfg.write_app_list()
