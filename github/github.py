a5126c74f60b02d0a91a004e0247e53ee4825f98
FroyoXSG <4962476+FroyoXSG@users.noreply.github.com>
2021-07-14 23:36:19 +0200

    fix f-string without parameter

--- github/github.py
+++ github/github.py
@@ -5,7 +5,7 @@ This Source Code Form is subject to the terms of the Mozilla Public License, v. 
 If a copy of the MPL was not distributed with this file,
 You can obtain one at https://mozilla.org/MPL/2.0/.
 """
-GITHUB_DATE = f"%Y-%m-%dT%H:%M:%SZ"
+GITHUB_DATE = "%Y-%m-%dT%H:%M:%SZ"
 HEADERS = {
     "Accept": "application/vnd.github.v3+json",
     "Authorization": "",
